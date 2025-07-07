7deba1ebb64a2d645e767c29a6532521fbfd7507
moyellamahitha <moyellamahitha123@gmail.com>
2025-07-07 12:45:15 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3054;
+const port = 3207;
 
 // Database connection configuration
 const pool = new Pool({
     user: 'postgres',
     host: 'postgres',
     database: 'offboarding_db',
-    password: 'admin834',
+    password: 'admin123',
     port: 5432,
 });
 
@@ -208,5 +208,5 @@ app.delete('/api/offboarding', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://54.81.30.45:${port}`);
+    console.log(`Server running at http://3.85.61.23:${port}`);
 });
